d5c904f505e957165f00838bd92be55117930be1
ch9eri <limch9eri@gmail.com>
2022-11-10 02:36:28 +0900

    [FEAT] api 파일

---
+++ nextflix/pages/api/api.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const api = axios.create({
+  BASE_URL: 'https://api.themoviedb.org/3/movie/',
+  params: {
+    api_key: 'f85ba1745021cb0f98ac340407ad592b',
+  }
+});
+
+export async function getNowPlaying() {
+  const nowPlaying = await api.get('now_playing');
+  return nowPlaying;
+};
+
+export async function getPopular() {
+  const popular = await api.get('popular');
+  return popular;
+};
+
+export async function getTopRated() {
+  const topRated = await api.get('top_rated');
+  return topRated;
+};
+
+export async function getUpcoming() {
+  const upcoming = await api.get('upcoming');
+  return upcoming;
+};
