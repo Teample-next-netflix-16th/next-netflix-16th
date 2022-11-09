484108ce04d7a55f8dba8cc45ecb7becf9091da4
ch9eri <limch9eri@gmail.com>
2022-11-10 04:58:30 +0900

    feat: data 출력 + aapi 업데이트

--- nextflix/pages/api/api.js
+++ nextflix/pages/api/api.js
@@ -4,26 +4,35 @@ const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/movie/',
   params: {
     api_key: 'f85ba1745021cb0f98ac340407ad592b',
-    
   }
 });
 
+export async function getLatest() {
+  const latest = await api.get('latest?');
+  return latest;
+};
+
 export async function getNowPlaying() {
-  const nowPlaying = await api.get('now_playing');
+  const nowPlaying = await api.get('now_playing?');
   return nowPlaying;
 };
 
 export async function getPopular() {
-  const popular = await api.get('popular');
+  const popular = await api.get('popular?');
   return popular;
 };
 
 export async function getTopRated() {
-  const topRated = await api.get('top_rated');
+  const topRated = await api.get('top_rated?');
   return topRated;
 };
 
 export async function getUpcoming() {
-  const upcoming = await api.get('upcoming');
+  const upcoming = await api.get('upcoming?');
   return upcoming;
+};
+
+export async function getImg(movie_id) {
+  const poster = await api.get('${movie_id}/images?');
+  return poster;
 };
