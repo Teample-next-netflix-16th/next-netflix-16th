bc52b976f337746a279d4ea809fbb9f4202c1c59
ch9eri <limch9eri@gmail.com>
2022-11-10 04:55:17 +0900

    feat:   data  출력

--- nextflix/pages/Movies/NowPlaying/index.js
+++ nextflix/pages/Movies/NowPlaying/index.js
@@ -1,18 +1,27 @@
 import axios from "axios";
 import styled from "styled-components";
-import { getNowPlaying } from "../../api/api";
+import { getNowPlaying,getImg } from "../../api/api";
 
 
 export const getServerSideProps = async () =>{
     const res =  await getNowPlaying();
-    const data = res.data;
-
-  return { props: { data } }
+    const data = res.data.results;
+    return { props: { data } }
 }
 
 
 const NowPlaying =({data}) =>{
-    console.log(data);
+  return (
+  <div>
+    {data.map((movie) =>  (
+      <div>
+        <div>{movie.title}</div>
+        <img src={getImg(movie.id)} alt='now playing img' />
+      </div>
+    ))}
+  </div>
+  // console.log(data)
+  )
 }
 
 export default NowPlaying;
