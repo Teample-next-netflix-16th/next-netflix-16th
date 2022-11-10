efd845ade34768a8f7651aa5bf18f3fb1212ccca
ch9eri <limch9eri@gmail.com>
2022-11-11 00:29:38 +0900

    chore: 필요없는 css 제거

--- nextflix/components/ui/Layout/index.js
+++ nextflix/components/ui/Layout/index.js
@@ -4,12 +4,10 @@ import Footer from '../Footer'
 const Box = styled.div`
   position: relative;
   display: flex;
-  width: 30%;
+  width: 400px;
   height: 812px;
   //   추후에 100%로 수정 -> 걍 position을 absol로 바꿔야하나 ㄱ-
   background-color: black;
-  border-style: dotted;
-  border-color: white;
   margin: auto;
 
 
